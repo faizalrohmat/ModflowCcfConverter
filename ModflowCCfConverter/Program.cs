1f6e4a65bab77faac49a3a48bb9ee6f4cbe18bcb
Faizal Rohmat <immaddudin@gmail.com>
2016-01-06 12:58:00 -0700

    Tried this one: http://stackoverflow.com/questions/2030847/best-way-to-read-a-large-file-into-a-byte-array-in-c

--- ModflowCCfConverter/Program.cs
+++ ModflowCCfConverter/Program.cs
@@ -21,30 +21,13 @@ namespace ModflowCCfConverter
 
         public void DoProgram()
         {
-            //string inputAddress = @"D:\DSModel_09.ccf";
-            //string targetAddress = @"D:\DSModel_09.txt";
+            string inputAddress = @"D:\DSModel_09.ccf";
+            string targetAddress = @"D:\DSModel_09.txt";
 
-            char[] invalidPathChars = Path.GetInvalidPathChars();
-            MemoryStream memStream = new MemoryStream();
-            BinaryWriter binWriter = new BinaryWriter(memStream);
 
-            // Write to memory.
-            binWriter.Write("Invalid file path characters are: ");
-            binWriter.Write(Path.GetInvalidPathChars(), 0, Path.GetInvalidPathChars().Length);
+            byte[] collection = File.ReadAllBytes(inputAddress);
 
-            // Create the reader using the same MemoryStream 
-            // as used with the writer.
-            BinaryReader binReader = new BinaryReader(memStream);
-
-            // Set Position to the beginning of the stream.
-            memStream.Position = 0;
-
-            // Read the data from memory and write it to the console.
-            Console.Write(binReader.ReadString());
-            int arraySize = (int)(memStream.Length - memStream.Position);
-            char[] memoryData = new char[arraySize];
-            binReader.Read(memoryData, 0, arraySize);
-            Console.WriteLine(memoryData);
+            File.WriteAllBytes(targetAddress, collection);
 
             Console.ReadLine();
         }
