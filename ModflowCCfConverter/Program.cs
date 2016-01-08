b0f61e269b17066c77b73c73e102befd2bf1424e
Faizal Rohmat <immaddudin@gmail.com>
2016-01-07 17:04:12 -0700

    Revised Eric's work. Seems working! lol!

--- ModflowCCfConverter/Program.cs
+++ ModflowCCfConverter/Program.cs
@@ -22,15 +22,130 @@ namespace ModflowCcfConverter
         public void DoProgram()
         {
             string inputAddress = @"D:\DSModel_09.ccf";
+            string inputAddress2 = @"D:\ReModel3.ccf";
             string targetAddress = @"D:\DSModel_09.txt";
 
+            EricMethod(inputAddress2);
+
+            // MyMethod(inputAddress);
+            // FailedMethod(inputAddress);
+
+            Console.ReadLine();
+
+        }
+
+        private static void FailedMethod(string inputAddress)
+        {
+            string r = "r";
+            string c;
+
+            using (FileStream fs = new FileStream(inputAddress, FileMode.Open, FileAccess.Read))
+            {
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    for (int pos = 0; pos < fs.Length; pos++)
+                    {
+                        c = new string(br.ReadChars(1));
+                        if (r.CompareTo(c) == 0)
+                        {
+                            Console.WriteLine((fs.Position - (long)1).ToString() + "\t" + c.ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void MyMethod(string inputAddress)
+        {
+            using (FileStream fs = new FileStream(inputAddress, FileMode.Open, FileAccess.Read))
+            {
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    // File lengths
+                    Console.WriteLine("File stream length: " + fs.Length);
+                    Console.WriteLine();
+
+                    // timestep, and description
+                    Console.Write(fs.Position.ToString() + "\t");
+                    int timeStep = br.ReadInt32();
+                    Console.WriteLine("timeStep\t\t: " + timeStep.ToString());
+
+                    // stress period
+                    Console.Write(fs.Position.ToString() + "\t");
+                    int stressPeriod = br.ReadInt32();
+                    Console.WriteLine("stressPeriod\t\t: " + stressPeriod.ToString());
+
+                    Console.Write(fs.Position.ToString() + "\t");
+                    char[] fText = br.ReadChars(16);
+                    Console.WriteLine("description\t\t: " + new string(fText));
+
+
+
+                    // Number of columns
+                    Console.Write(fs.Position.ToString() + "\t");
+                    int nCol = br.ReadInt32();
+                    Console.WriteLine("nCol\t\t\t: " + nCol.ToString());
+
+                    // Number of rows
+                    Console.Write(fs.Position.ToString() + "\t");
+                    int nRow = br.ReadInt32();
+                    Console.WriteLine("nRow\t\t\t: " + nRow.ToString());
+
+                    // Number of layers
+                    Console.Write(fs.Position.ToString() + "\t");
+                    int nLay = Math.Abs(br.ReadInt32());
+                    Console.WriteLine("nLays\t\t\t: " + nLay.ToString());
+
+
+                    // type of data
+                    Console.Write(fs.Position.ToString() + "\t");
+                    int iType = br.ReadInt32();
+                    Console.WriteLine("iType\t\t\t: " + iType.ToString());
+
+                    // number of values per cell
+                    int nvalsPerCell = 1;
+                    Console.Write(fs.Position.ToString() + "\t");
+                    if (iType == 5)
+                    {
+                        nvalsPerCell = br.ReadInt32();
+                    }
+                    Console.WriteLine("nvalsPerCell\t\t: " + nvalsPerCell.ToString());
+
+                    // conditional, depending on the nvalsPerCell
+                    for (int y = 1; y <= nvalsPerCell - 1; y++)
+                    {
+                        Console.Write(fs.Position.ToString() + "\t");
+                        char[] tmpText = br.ReadChars(16);
+                        Console.WriteLine("tmpText\t\t: " + new string(tmpText));
+                    }
+
+                    // conditional, depending on nlist
+                    int nList = 0;
+                    if (iType == 2 || iType == 5)
+                    {
+                        Console.Write(fs.Position.ToString() + "\t");
+                        nList = br.ReadInt32();
+                        Console.WriteLine("nList\t\t\t: " + nList.ToString());
+                    }
+
+                    // skip if true
+                    bool SkipValues = false;
+
+
+                }
+            }
+        }
+
+        private static void EricMethod(string inputAddress)
+        {
+            int timeStepCount = 0;
             using (FileStream fs = new FileStream(inputAddress, FileMode.Open, FileAccess.Read))
             {
                 using (BinaryReader br = new BinaryReader(fs))
                 {
                     long streamLength = fs.Length;
 
-                    int timeStepCount, timeStep/*, stressPeriod*/;
+                    int timeStep/*, stressPeriod*/;
                     char[] fText, tmpText;
 
                     int nCol, nRow, nLay;
@@ -42,7 +157,9 @@ namespace ModflowCcfConverter
 
                     int beginningTS, endingTS;
 
-                    object[] layerFlux;
+                    object[] layerFlux = null;
+
+                    int cellID, layerID;
 
                     beginningTS = 1;
                     endingTS = 292;
@@ -59,6 +176,7 @@ namespace ModflowCcfConverter
                         Console.WriteLine("stressPeriod\t\t: " + timeStep.ToString());
                         fText = br.ReadChars(16);
                         Console.WriteLine("description\t\t: " + new string(fText));
+                        //Console.Read();
 
                         // Number of columns, rows, and layers
                         nCol = br.ReadInt32();
@@ -83,10 +201,10 @@ namespace ModflowCcfConverter
                         }
                         Console.WriteLine("nvalsPerCell\t\t: " + nvalsPerCell.ToString());
 
-                        for (int y = 1; y < nvalsPerCell - 1; y++)
+                        for (int y = 1; y <= nvalsPerCell - 1; y++)
                         {
                             tmpText = br.ReadChars(16);
-                            Console.WriteLine("tmpText\t\t: " + new string(tmpText));
+                            Console.WriteLine("tmpText\t\t\t: " + new string(tmpText));
                         }
 
                         int nList = 0;
@@ -115,12 +233,14 @@ namespace ModflowCcfConverter
                             case "FLOW RIGHT FACE ":
                             case "FLOW FRONT FACE ":
                             case "FLOW LOWER FACE ":
+                            case "         STORAGE":
+                                //case "           WELLS":
                                 skipValues = true;
                                 break;
                             default:
                                 break;
                         }
-                        Console.WriteLine("skipValues\t\t: "+ skipValues.ToString());
+                        Console.WriteLine("skipValues\t\t: " + skipValues.ToString());
 
                         if (!skipValues)
                         {
@@ -130,18 +250,120 @@ namespace ModflowCcfConverter
 
                             for (int i = 0; i < nLay; i++)
                             {
-                                layerFlux[i] = new float[nCol, nRow];
+                                layerFlux[i] = new float[nCol * nRow];
 
                             }
                         }
 
-                    } while (/*fs.Position < fs.Length*/false);
+                        // read the values from the files
+                        switch (iType)
+                        {
+                            case 0:
+                            case 1:
+                                Console.WriteLine("3D array of values");
+                                if (skipValues)
+                                {
+                                    fs.Position += nLay * nRow * nCol * 4;
+                                    Console.WriteLine("[Values skipped]");
+                                }
+                                else
+                                {
+                                    for (int layer = 1; layer <= nLay; layer++)
+                                    {
+                                        for (int row = 1; row <= nRow; row++)
+                                        {
+                                            for (int col = 1; col <= nCol; col++)
+                                            {
+                                                Console.WriteLine(br.ReadSingle().ToString() + "\t");
+                                            }
+                                        }
+                                    }
+                                }
+                                break;
+                            case 2:
+                            case 5:
+                                Console.WriteLine("list of cells and associated values");
+                                if (skipValues)
+                                {
+                                    fs.Position += nList * (4 + nvalsPerCell * 4);
+                                    Console.WriteLine("[Values skipped]");
+                                }
+                                else
+                                {
+                                    Console.Write("layerID" + "\t" + "cellID");
+                                    for (int i = 1; i <= nvalsPerCell; i++) Console.Write("\t" + "val");
+                                    Console.WriteLine();
 
+                                    for (int y = 1; y <= nList; y++)
+                                    {
+                                        cellID = br.ReadInt32();
+                                        layerID = ((cellID - ((cellID - 1) % (nCol * nRow) + 1)) / (nCol * nRow) + 1); //layerid starts at one
+                                        Console.Write(layerID + "\t" + cellID);
+                                        for (int i = 1; i <= nvalsPerCell; i++)
+                                        {
+                                            Console.Write("\t" + br.ReadSingle().ToString());
+                                        }
+                                        Console.WriteLine();
+                                    }
+                                }
+                                break;
+                            case 3:
+                                Console.WriteLine("2D layer indicator array followed by a 2D array of values");
+                                if (skipValues)
+                                {
+                                    fs.Position += nRow * nCol * 4 * 2;
+                                    Console.WriteLine("[Values skipped]");
+                                }
+                                else
+                                {
+                                    int[] layerIDs = new int[nCol * nRow];
+                                    for (int row = 1; row <= nRow; row++)
+                                    {
+                                        for (int col = 1; col <= nCol; col++)
+                                        {
+                                            layerIDs[(row - 1) * nCol + col] = br.ReadInt32();
+                                            Console.WriteLine("layerIDs: " + layerIDs.ToString());
+                                        }
+                                    }
+                                    for (int row = 1; row <= nRow; row++)
+                                    {
+                                        for (int col = 1; col <= nCol; col++)
+                                        {
+                                            cellID = (row - 1) * nCol + col;
+                                            Console.WriteLine(layerIDs[cellID].ToString() + br.ReadSingle().ToString());
+                                        }
+                                    }
+                                }
+                                break;
+                            case 4:
+                            default:
+                                Console.WriteLine("'2D array of values associated with layer 1");
+                                if (skipValues)
+                                {
+                                    fs.Position += nRow * nCol * 4;
+                                    Console.WriteLine("[Values skipped]");
+                                }
+                                else
+                                {
+                                    for (int row = 1; row <= nRow; row++)
+                                    {
+                                        for (int col = 1; col <= nCol; col++)
+                                        {
+                                            cellID = (row - 1) * nCol + col;
+                                            Console.WriteLine("cellID: " + cellID + "\t" + br.ReadSingle());
+                                        }
+                                    }
+                                }
+                                break;
+                        }
 
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine();
+                        //Console.ReadLine();
+                    } while (fs.Position < fs.Length);
                 }
             }
-            Console.ReadLine();
-
+            Console.WriteLine(timeStepCount);
         }
     }
 }
